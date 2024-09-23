d9c320afabaf41d3d0836de14c6a5e766029a83e
Society42 <104001559+Society42@users.noreply.github.com>
2024-09-23 20:38:45 +0100

    Delete src directory

--- src/Schemas.js/modroleSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const guildSettingsSchema = new mongoose.Schema({
-    guildID: { type: String, required: true, unique: true },
-    modRole: { type: String, default: null },
-});
-
-module.exports = mongoose.model('GuildSettings', guildSettingsSchema);
